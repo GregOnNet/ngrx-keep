62e0a1fb1c81df3cca7ac796a3b4095b118036b5
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-06-21 16:07:42 +0200

    ref(notes): get rid of switch-case statement

--- src/app/notes/store/reducers/notes.reducer.ts
+++ src/app/notes/store/reducers/notes.reducer.ts
@@ -2,6 +2,7 @@ import { createEntityAdapter, EntityState } from '@ngrx/entity';
 
 import { Note } from '../../models/note';
 import { NotesActions, NotesActionTypes } from '../actions/notes.actions';
+import { Action, ActionReducerMap } from '@ngrx/store';
 
 export interface State extends EntityState<Note> {}
 
@@ -18,18 +19,30 @@ export const {
 
 const initialState = notes.getInitialState();
 
+export interface ActionWithPayload {
+  type: string;
+  payload?: any;
+}
+
+export interface ActionHandlers<TState> {
+  [actionType: string]: (payload, state: TState) => TState;
+}
+
+const actionHandlers: ActionHandlers<State> = {
+  [NotesActionTypes.CreateNote]: notes.addOne,
+  [NotesActionTypes.LoadNotesSuccessful]: notes.addMany
+};
+
 export function reducer(state = initialState, action: NotesActions): State {
-  switch (action.type) {
-    case NotesActionTypes.CreateNote: {
-      return notes.addOne(action.payload, state);
-    }
+  return createReducer(actionHandlers)(state, action);
+}
 
-    case NotesActionTypes.LoadNotesSuccessful: {
-      return notes.addMany(action.payload, state);
-    }
-
-    default: {
-      return state;
-    }
-  }
+export function createReducer<TState>(
+  handlers: ActionHandlers<TState>
+): (state: TState, action: ActionWithPayload) => TState {
+  return (state: TState, action: ActionWithPayload) => {
+    return !!handlers[action.type]
+      ? handlers[action.type](action.payload, state)
+      : state;
+  };
 }
