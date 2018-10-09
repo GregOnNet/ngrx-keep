6de52537ca7a4ef7f7a740fcdf8a3a7370b3346f
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-10-09 17:16:56 +0200

    comment ngrx-data usage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import {
   RouterStateSerializer,
   StoreRouterConnectingModule
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -2,12 +2,20 @@ import { Component } from '@angular/core';
 
 import { NotesBoard } from '../notes/store/data/notes-board.service';
 
+import * as fromNotes from './store';
+import { Store } from '@ngrx/store';
+import { LoadAllNotes } from './store';
+
 @Component({
   selector: 'nt-notes',
   template: `<router-outlet></router-outlet>`
 })
 export class NotesComponent {
-  constructor(private notes: NotesBoard) {
-    this.notes.getAll();
+  constructor(
+    private store: Store<fromNotes.State>,
+    private notes: NotesBoard
+  ) {
+    this.store.dispatch(new LoadAllNotes());
+    // this.notes.getAll();
   }
 }
